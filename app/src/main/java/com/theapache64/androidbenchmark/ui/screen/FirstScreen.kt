9fab8b41273883cc1a6650f91d9e047ac52e8118
theapache64 <theapache64@gmail.com>
2023-07-29 23:21:25 +0530

    🌟 add render actions and corresponding composables

--- app/src/main/java/com/theapache64/androidbenchmark/ui/screen/FirstScreen.kt
+++ app/src/main/java/com/theapache64/androidbenchmark/ui/screen/FirstScreen.kt
@@ -2,7 +2,6 @@ package com.theapache64.androidbenchmark.ui.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
@@ -12,51 +11,85 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.theapache64.androidbenchmark.R
-import kotlin.system.measureTimeMillis
 
 @Composable
-fun SplashScreen(
-    onSplashFinished: () -> Unit,
+fun FirstScreen(
+    renderAction: RenderAction,
+    onFirstScreenFinished: () -> Unit, // TODO:
 ) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        CoilSvg()
+        when (renderAction) {
+            RenderAction.VECTOR_COIL -> VectorCoil()
+            RenderAction.VECTOR_COMPOSE -> VectorCompose()
+            RenderAction.PNG_COIL -> PngCoil()
+            RenderAction.PNG_COMPOSE -> PngCoil()
+            RenderAction.WEBP_COIL -> WebPCoil()
+            RenderAction.WEBP_COMPOSE -> WebPCompose()
+        }
     }
 }
 
 // Phase - 1
-// SVG - Coil
+// Vector - Coil
 @Composable
-fun CoilSvg() {
-    Column {
-        measureTimeMillis {
-            AsyncImage(
-                model = R.drawable.mexicano_vector,
-                modifier = Modifier.size(300.dp, 300.dp),
-                contentDescription = null,
-            )
-        }.let {
-            println(":CoilSvg: took $it ms")
-        }
-
-        measureTimeMillis {
-            Image(
-                painter = painterResource(id = R.drawable.mexicano_vector),
-                modifier = Modifier.size(300.dp, 300.dp),
-                contentDescription = null,
-            )
-        }.let {
-            println(":Image: took $it ms")
-        }
-    }
+fun VectorCoil() {
+    AsyncImage(
+        model = R.drawable.mexicano_vector,
+        modifier = Modifier.size(300.dp, 300.dp),
+        contentDescription = null,
+    )
 }
-// SVG - Compose
+
+// Vector - Compose
+@Composable
+fun VectorCompose() {
+    Image(
+        painter = painterResource(id = R.drawable.mexicano_vector),
+        modifier = Modifier.size(300.dp, 300.dp),
+        contentDescription = null,
+    )
+}
 
 // PNG - Coil
+@Composable
+fun PngCoil() {
+    AsyncImage(
+        model = R.drawable.mexicano_png,
+        modifier = Modifier.size(300.dp, 300.dp),
+        contentDescription = null,
+    )
+}
+
 // PNG - Compose
+@Composable
+fun PngCompose() {
+    Image(
+        painter = painterResource(id = R.drawable.mexicano_png),
+        modifier = Modifier.size(300.dp, 300.dp),
+        contentDescription = null,
+    )
+}
 
 // Phase - 2
 // WebP - Coil
+@Composable
+fun WebPCoil() {
+    AsyncImage(
+        model = R.drawable.mexicano_webp,
+        modifier = Modifier.size(300.dp, 300.dp),
+        contentDescription = null,
+    )
+}
+
 // WebP - Compose
+@Composable
+fun WebPCompose() {
+    Image(
+        painter = painterResource(id = R.drawable.mexicano_webp),
+        modifier = Modifier.size(300.dp, 300.dp),
+        contentDescription = null,
+    )
+}
--- app/src/main/java/com/theapache64/androidbenchmark/ui/screen/MainActivity.kt
+++ app/src/main/java/com/theapache64/androidbenchmark/ui/screen/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.theapache64.androidbenchmark.ui.screen
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,36 +16,65 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
-import coil.compose.AsyncImage
-import com.theapache64.androidbenchmark.R
 import com.theapache64.androidbenchmark.ui.theme.Android_BenchmarkTheme
+import java.io.Serializable
+
+enum class RenderAction {
+    VECTOR_COIL,
+    VECTOR_COMPOSE,
+    PNG_COIL,
+    PNG_COMPOSE,
+    WEBP_COIL,
+    WEBP_COMPOSE
+}
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private const val KEY_RENDER_ACTION = "render_action"
+        fun createIntent(context: Context, renderAction: RenderAction): Intent {
+            return Intent(context, MainActivity::class.java).apply {
+                putExtra(KEY_RENDER_ACTION, renderAction)
+            }
+        }
+    }
 
     @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val renderAction = intent.serializable(KEY_RENDER_ACTION)
+            ?: RenderAction.PNG_COIL // default action
+
         setContent {
             Android_BenchmarkTheme {
                 Surface(modifier = Modifier.semantics {
                     testTagsAsResourceId = true
                 }) {
-                    AppNavigation()
+                    AppNavigation(renderAction = renderAction)
                 }
             }
         }
     }
 
     @Composable
-    private fun AppNavigation() {
-        var isSplashFinished by remember { mutableStateOf(false) }
-        if (isSplashFinished) {
+    private fun AppNavigation(renderAction: RenderAction) {
+        var isFirstScreenFinished by remember { mutableStateOf(false) }
+        if (isFirstScreenFinished) {
             SecondScreen()
         } else {
-            SplashScreen(onSplashFinished = {
-                isSplashFinished = true
-            })
+            FirstScreen(
+                renderAction = renderAction,
+                onFirstScreenFinished = {
+                    isFirstScreenFinished = true
+                }
+            )
         }
+    }
+
+    inline fun <reified T : Serializable> Intent.serializable(key: String): T? = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializableExtra(key, T::class.java)
+        else -> @Suppress("DEPRECATION") getSerializableExtra(key) as? T
     }
 
 
