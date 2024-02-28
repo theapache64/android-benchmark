1398bc974cb29c1f3424d57d22c5ad8170961db7
theapache64 <theapache64@gmail.com>
2024-02-29 00:50:25 +0530

    ⬆️ bump benchmark libs

--- benchmark/src/main/java/com/theapache64/androidbenchmark/benchmarker/AndroidBenchmark.kt
+++ benchmark/src/main/java/com/theapache64/androidbenchmark/benchmarker/AndroidBenchmark.kt
@@ -2,6 +2,7 @@ package com.theapache64.androidbenchmark.benchmarker
 
 import androidx.benchmark.macro.FrameTimingMetric
 import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -25,13 +26,16 @@ class AndroidBenchmark {
     fun startup() = benchmarkRule.measureRepeated(
         packageName = "com.theapache64.androidbenchmark",
         metrics = listOf(
-            FrameTimingMetric()
+            FrameTimingMetric(),
+            StartupTimingMetric()
         ),
-        iterations = 15,
+        iterations = 5,
         startupMode = StartupMode.COLD,
         setupBlock = {
             // Press home button before each run to ensure the starting activity isn't visible.
             pressHome()
+
+
         }
     ) {
         // starts default launch activity
