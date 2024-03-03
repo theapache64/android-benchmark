1d118e3535e62272a4b442f13da745fc9c7455c3
theapache64 <theapache64@gmail.com>
2024-03-03 22:54:40 +0530

    ◀️ backup

--- benchmark/src/main/java/com/theapache64/androidbenchmark/benchmarker/LottieVsRiveBenchmark.kt
+++ benchmark/src/main/java/com/theapache64/androidbenchmark/benchmarker/LottieVsRiveBenchmark.kt
@@ -4,19 +4,16 @@ import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.github.theapache64.commonkeys.BenchmarkType
-import com.github.theapache64.commonkeys.LottieKeys
 import com.github.theapache64.commonkeys.LottieVsRiveKeys
 import com.theapache64.androidbenchmark.benchmarker.util.launchActivity
 import com.theapache64.androidbenchmark.benchmarker.util.waitForUiElement
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class LottievsRiveBenchmark {
+class LottieVsRiveBenchmark {
     @get:Rule
     val benchmarkRule = MacrobenchmarkRule()
 
@@ -38,7 +35,7 @@ private fun MacrobenchmarkRule.launchLottieVsRive(
 ) {
     // starts default launch activity
     waitForUiElement(
-        testTag = LottieKeys.TAG_DONE,
+        testTag = LottieVsRiveKeys.TAG_DONE,
         onFound = { tag ->
             // do nothing
         },
